707e184bee45f86cc0664aa71e0390d752d9b48b
Gerardo <gerardomty2006@gmail.com>
2026-04-05 14:17:20 -0600

    Nothing much

--- TUI/test.py
+++ TUI/test.py
@@ -1,3 +1,4 @@
+from enum import Enum, IntEnum
 import ui
 from ui import ButtonFocusable, ButtonGlobalKey, Input, Label,Layout, Push,Screen,ScreenHandler,Where,ReusableActions
 import curses
@@ -17,6 +18,13 @@ ui.logger = logger
 
 
 
+class MC(IntEnum):
+    BTNNORMAL = 0
+    BTNPRESSED = 1
+
+    
+
+
 
 class Program:
 
@@ -25,16 +33,30 @@ class Program:
 
 
     def changefg(self):
-        colors = [curses.COLOR_WHITE,curses.COLOR_BLUE,curses.COLOR_CYAN]
+        # colors = [curses.COLOR_WHITE,curses.COLOR_BLUE,curses.COLOR_CYAN]
+        colors = [1,2,3]
         c = 0
+        off = True
         def loop():
-            nonlocal c
+            nonlocal c,off
             while (True):
                 logger.info("running")
                 time.sleep(1)
                 color = colors[c]
-                curses.init_pair(1,curses.COLOR_WHITE,color)
+
+                self.win.bkgd(" ",curses.color_pair(color))
+                # curses.init_pair(1,color,curses.COLOR_WHITE)
+
+                
+                # if off:
+                #     self.win.chgat(0,0,curses.A_UNDERLINE | curses.color_pair(1))
+                # else:
+                #     self.win.chgat(0,0,curses.A_DIM | curses.color_pair(1))
+
+                # off = not off
+
                 self.win.refresh()
+
 
                 c += 1
                 if c >= len(colors):
@@ -49,23 +71,33 @@ class Program:
             curses.start_color()
 
         curses.has_extended_color_support()
+        curses.use_default_colors()
 
 
-        curses.init_color(1,238, 220, 247)
+
+
+        # curses.init_color(1,238, 220, 247)
         # curses.use_default_colors()
-        curses.init_pair(1,curses.COLOR_BLUE,1)
-        curses.init_pair(2, 0, 250)
-        # curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED)
-        curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_GREEN)
-        curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_MAGENTA)
-        curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_YELLOW)
-        curses.init_pair(6, 200, 100)
+
+        curses.init_pair(MC.BTNNORMAL,curses.COLOR_WHITE,-1)
+        curses.init_pair(MC.BTNPRESSED,curses.COLOR_WHITE,curses.COLOR_RED)
+
+
+        # curses.init_pair(1,curses.COLOR_BLUE,-1)
+        # curses.init_pair(2, 0, 250)
+        # # curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED)
+        # curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_GREEN)
+        # curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_MAGENTA)
+        # curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_YELLOW)
+        # curses.init_pair(6, 200, 100)
     
         
         
 
         logger.info(f"has colors {curses.has_colors()}, can change color {curses.can_change_color()}, max color {curses.COLORS} extender colors {curses.has_extended_color_support()} longname {curses.longname()}")
         logger.info(f"Color content {curses.color_content(1)}")
+
+        logger.info(f"shit { curses.termattrs()}")
 
 
         logger.info(f"COlors are : ?{ curses.COLOR_WHITE,curses.COLOR_BLUE }")
@@ -74,11 +106,20 @@ class Program:
 
 
 
+    def create_screens(self,stdsrc):
+        self.screenHandler = ScreenHandler(stdsrc)
+        self.reusableActions = ReusableActions(self.screenHandler)
+
+        self.initial_layout(stdsrc)
+
 
 
         
 
-    def initial_layout(self):
+    def initial_layout(self,stdsrc):
+
+        self.screenHandler.set_spotlight(screen_name_identifier="initial_layout")
+
 
         screen = Screen("initial_layout")
         layout = Layout(focusable=True,hasBorder=True,axis='vertical',where=Where.CENTER_OF_SCREEN)
@@ -102,40 +143,96 @@ class Program:
 
         
 
+    def tests(self,stdsrc):
+
+        # self.changefg()
+        win = curses.newwin(10,10,2,3)
+        win2 = curses.newwin(10,10,6,20)
+        self.win = win
+        # win.addstr("hi")
+        win.bkgd(" ",curses.color_pair(MC.BTNNORMAL))
+        win.box()
+        win.addstr("buenas",curses.A_DIM)
+        win.refresh()
+
+        # win2.bkgd(" ",curses.color_pair(2))
+
+        win2.attrset(curses.color_pair(2))
+
+        win2.addstr("helllo")
+        win2.chgat(0,0,curses.A_UNDERLINE | curses.color_pair(MC.BTNNORMAL))
+
+        # curses.curs_set(0)
 
 
-    def main(self,stdsrc:curses.window):
+
+        # win2.chgat(0,0,2,curses.A_UNDERLINE | curses.color_pair(2))
+        # win2.chgat(0,2,1,curses.A_UNDERLINE | curses.color_pair(3))
+        # win2.chgat(0,3,1,curses.A_UNDERLINE | curses.color_pair(4))
+
+
+
+
+
+        win2.refresh()
+
+
+        curses.setsyx(0, 0)
+
+
+
+
+    def main(
+        self,
+        stdsrc:curses.window,
+    ):
 
         self.init_colors()
         stdsrc.timeout(100)
         stdsrc.refresh()
+
+
+        self.tests(stdsrc)
         
-        self.changefg()
-        win = curses.newwin(5,5,2,3)
-        win2 = curses.newwin(5,5,6,20)
-        self.win = win
-        # win.addstr("hi")
-        win.bkgd(" ",curses.color_pair(1))
-        win.addch(curses.ACS_BULLET,curses.A_ITALIC)
-        win.refresh()
-
-        win2.bkgd(" ",curses.color_pair(1))
-        win2.addch("h")
-        win2.refresh()
-
-        # curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_GREEN)
 
 
-        # self.screenHandler = ScreenHandler(stdsrc)
-        # self.reusableActions = ReusableActions(self.screenHandler)
-        # self.initial_layout()
-        # self.screenHandler.set_spotlight(screen_name_identifier="initial_layout")
 
 
+        a = 0
+        buttonpressed = False
+        button_elapsed = 0
+        animation_miliseconds = 1000
         while(1):
+            start = time.perf_counter()
             c = stdsrc.getch()
+            end = time.perf_counter()
+
+            if buttonpressed:
+                elapsed = ( end-start ) * 1000
+                button_elapsed += elapsed
+
+                logger.info(f"btnelapsed animationml {button_elapsed} {animation_miliseconds}")
+                if button_elapsed >= animation_miliseconds:
+                    buttonpressed = False
+                    button_elapsed = 0
+                    self.win.bkgd(" ",curses.color_pair(MC.BTNNORMAL))
+                    self.win.refresh()
+
+
+            # logger.info(f"Time took {end-start}")
+
+
             if c == -1:
                 continue
+
+            if c == ord( '\n' ):
+                logger.info("Pressed?")
+                buttonpressed = True
+                button_elapsed = 0
+                self.win.bkgd(" ",curses.color_pair(MC.BTNPRESSED))
+                self.win.refresh()
+
+                
 
             # self.screenHandler.handleKey(c)
 
