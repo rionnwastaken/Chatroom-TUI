f79b009159d045c92c4cc7f13100645d04283fcf
Gerardo <gerardomty2006@gmail.com>
2026-04-10 12:41:12 -0600

    Example file that demonstrates layouts and traversing '\n' when login has focus just hides the screen

--- TUI/test.py
+++ TUI/test.py
@@ -1,21 +1,29 @@
 from enum import Enum, IntEnum
-import ui
-from ui import ButtonFocusable, ButtonGlobalKey, Input, Label,Layout, Push,Screen,ScreenHandler,Where,ReusableActions
 import curses
 import logging
 import _thread
 import time
-
+import ui
+from ui import ButtonFocusable, ButtonGlobalKey, FocusableLayout, GlobalKeyLayout, Input, Label,Layout, Push,Screen,ScreenHandler,Where,ReusableActions
 
 
 with open("program.log","w") as f:
     f.write("")
 
+# exit(0)
+
+
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO,filename="program.log")
-ui.logger = logger
 
+logger.info("Logger active?")
+
+
+
+
+ui.logger = logger
 
 
 class MC(IntEnum):
@@ -44,7 +52,9 @@ class Program:
                 time.sleep(1)
                 color = colors[c]
 
-                self.win.bkgd(" ",curses.color_pair(color))
+                # self.win.bkgd(" ",curses.color_pair(color))
+                # self.win.erase()
+
                 # curses.init_pair(1,color,curses.COLOR_WHITE)
 
                 
@@ -66,12 +76,16 @@ class Program:
 
 
     def init_colors(self):
+        
+
 
         if curses.has_colors():
             curses.start_color()
 
-        curses.has_extended_color_support()
         curses.use_default_colors()
+
+        logger.info(f"Color black {curses.COLOR_BLACK}")
+        logger.info(f"Color white {curses.COLOR_WHITE}")
 
 
 
@@ -79,17 +93,14 @@ class Program:
         # curses.init_color(1,238, 220, 247)
         # curses.use_default_colors()
 
-        curses.init_pair(MC.BTNNORMAL,curses.COLOR_WHITE,-1)
-        curses.init_pair(MC.BTNPRESSED,curses.COLOR_WHITE,curses.COLOR_RED)
+        ui.DefaultColors.init()
 
 
-        # curses.init_pair(1,curses.COLOR_BLUE,-1)
-        # curses.init_pair(2, 0, 250)
-        # # curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED)
-        # curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_GREEN)
-        # curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_MAGENTA)
-        # curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_YELLOW)
-        # curses.init_pair(6, 200, 100)
+        # curses.init_pair(MC.BTNNORMAL,curses.COLOR_WHITE,-1)
+        # curses.init_pair(MC.BTNPRESSED,curses.COLOR_YELLOW,-1)
+        # curses.init_pair(2,curses.COLOR_GREEN,-1)
+        # curses.init_pair(3,curses.COLOR_BLUE,-1)
+
     
         
         
@@ -108,9 +119,31 @@ class Program:
 
     def create_screens(self,stdsrc):
         self.screenHandler = ScreenHandler(stdsrc)
-        self.reusableActions = ReusableActions(self.screenHandler)
+        self.ra = ReusableActions(self.screenHandler)
 
         self.initial_layout(stdsrc)
+
+
+    def createLayout(self,screen,topy,topx):
+
+        layout = FocusableLayout(hasBorder=True,axis='vertical',coordinates={"topy":topy,"topx":topx})
+        signup = ButtonFocusable(text='Sign up',push=Push(0,0,2,0),hasBorder=True)
+        login = ButtonFocusable(text='Log in',hasBorder=True)
+
+
+        screen.add_layout(layout)
+        layout.add_item(signup)
+        layout.add_item(login)
+
+        login.addAction(callable=self.ra.changeColor3(login,[1,0,3]),key="\n")
+
+    
+        def clearScreen():
+            screen.get_window().erase()
+            screen.get_window().refresh()
+            
+
+        login.addAction(callable=clearScreen,key="\n")
 
 
 
@@ -118,18 +151,36 @@ class Program:
 
     def initial_layout(self,stdsrc):
 
-        self.screenHandler.set_spotlight(screen_name_identifier="initial_layout")
 
 
         screen = Screen("initial_layout")
-        layout = Layout(focusable=True,hasBorder=True,axis='vertical',where=Where.CENTER_OF_SCREEN)
+        layout = FocusableLayout(hasBorder=True,axis='vertical',where=Where.CENTER_OF_SCREEN)
 
-        label = Label("hi")
-        signup = ButtonFocusable(text='Sign up',background=2,push=Push(0,0,2,0),hasBorder=True)
-        login = ButtonFocusable(text='Log in',background=2,hasBorder=True)
-
+        signup = ButtonFocusable(text='Sign up',push=Push(0,0,2,0),hasBorder=True)
+        login = ButtonFocusable(text='Log in',hasBorder=True)
 
 
+        def escpressed():
+            logger.info("esc was pressed")
+
+        layout.register_global_key(27,escpressed)
+
+        layout2 = GlobalKeyLayout(hasBorder=True,coordinates={'topx':0,'topy':0})
+        btn = ButtonGlobalKey(text="hola",hasBorder=True,global_key_char='a')
+
+        def hi():
+            logger.info("hi")
+
+        btn.addAction(hi)
+
+
+        layout2.add_item(btn)
+        screen.add_layout(layout2)
+
+
+
+        self.createLayout(screen,10,20)
+        self.createLayout(screen,10,30)
 
 
         
@@ -139,7 +190,33 @@ class Program:
         layout.add_item(signup)
         layout.add_item(login)
 
+        login.addAction(callable=self.ra.changeColor3(login,[1,0,3]),key="\n")
+
+    
+        def clearScreen():
+            screen.get_window().erase()
+            screen.get_window().refresh()
+            
+
+        login.addAction(callable=clearScreen,key="\n")
+
         self.screenHandler.add_screen(screen)
+
+
+        self.screenHandler.set_spotlight(screen_name_identifier="initial_layout")
+
+
+        while(1):
+            start = time.perf_counter()
+            c = stdsrc.getch()
+            end = time.perf_counter()
+
+
+            if c == -1:
+                continue
+
+            self.screenHandler.handleKey(c)
+
 
         
 
@@ -151,57 +228,32 @@ class Program:
         self.win = win
         # win.addstr("hi")
         win.bkgd(" ",curses.color_pair(MC.BTNNORMAL))
-        win.box()
-        win.addstr("buenas",curses.A_DIM)
+        win.border('#','#','#','#','#','#','#','#',)
+
+        curses.init_pair(200,curses.COLOR_RED,-1)
+        curses.init_pair(201,curses.COLOR_WHITE,-1)
+        curses.init_pair(202,curses.COLOR_WHITE,-1)
+        win.addstr(1,1,"buenas",  curses.color_pair(1))
+        # win.chgat(1,1,200)
+
         win.refresh()
 
-        # win2.bkgd(" ",curses.color_pair(2))
-
-        win2.attrset(curses.color_pair(2))
-
-        win2.addstr("helllo")
-        win2.chgat(0,0,curses.A_UNDERLINE | curses.color_pair(MC.BTNNORMAL))
-
-        # curses.curs_set(0)
-
-
-
-        # win2.chgat(0,0,2,curses.A_UNDERLINE | curses.color_pair(2))
-        # win2.chgat(0,2,1,curses.A_UNDERLINE | curses.color_pair(3))
-        # win2.chgat(0,3,1,curses.A_UNDERLINE | curses.color_pair(4))
 
 
 
 
 
-        win2.refresh()
+
+        # win2.refresh()
 
 
-        curses.setsyx(0, 0)
-
-
-
-
-    def main(
-        self,
-        stdsrc:curses.window,
-    ):
-
-        self.init_colors()
-        stdsrc.timeout(100)
-        stdsrc.refresh()
-
-
-        self.tests(stdsrc)
-        
-
-
+        # curses.setsyx(0, 0)
 
 
         a = 0
         buttonpressed = False
         button_elapsed = 0
-        animation_miliseconds = 1000
+        animation_miliseconds = 200
         while(1):
             start = time.perf_counter()
             c = stdsrc.getch()
@@ -230,11 +282,84 @@ class Program:
                 buttonpressed = True
                 button_elapsed = 0
                 self.win.bkgd(" ",curses.color_pair(MC.BTNPRESSED))
+                # self.win.erase()
                 self.win.refresh()
 
                 
 
             # self.screenHandler.handleKey(c)
+
+
+    
+
+    def e(self,stdsrc:curses.window):
+
+        def _getcolor():
+            colors = [2,3,1]
+            pos = 0
+
+            def i():
+                nonlocal pos
+                color = colors[pos]
+                pos += 1
+                if pos >= len(colors):
+                    pos = 0
+
+                return color
+            return i
+
+        getcolor = _getcolor()
+
+        win = curses.newwin(10,10,3,5)
+        win.box()
+
+
+        win2 = win.derwin(4,4,0,0)
+        win2.addstr("he")
+        win2.box()
+
+        curses.init_pair(4,curses.COLOR_GREEN,-1)
+        curses.init_pair(5,curses.COLOR_WHITE,-1)
+
+        win2.bkgd(" ",curses.color_pair(1))
+        # win.bkgd(" ",curses.color_pair(0))
+        win.refresh()
+        win2.refresh()
+
+        while(1):
+            start = time.perf_counter()
+            c = stdsrc.getch()
+            end = time.perf_counter()
+
+
+            if c == -1:
+                continue
+
+            if c == ord('\n'):
+                win.bkgd(" ",curses.color_pair(getcolor()))
+                win.refresh()
+
+
+
+
+
+    def main(
+        self,
+        stdsrc:curses.window,
+    ):
+
+        self.init_colors()
+        stdsrc.timeout(100)
+        stdsrc.refresh()
+
+        # self.e(stdsrc)
+
+        # self.tests(stdsrc)
+        self.create_screens(stdsrc)
+        
+
+
+
 
 
 
